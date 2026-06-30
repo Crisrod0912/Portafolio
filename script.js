824a68decc77672d97bb28df73f12b3524978558
Crisrod0912 <rodriguezfernandezcristopher@gmail.com>
2026-06-29 18:08:57 -0600

    Update portafolio

---
+++ script.js
@@ -0,0 +1,29 @@
+function scrollToProjects() {
+    document.getElementById("projects").scrollIntoView({
+        behavior: "smooth"
+    })
+}
+
+let currentGallery = [];
+let currentIndex = 0;
+
+function openGallery(project) {
+    currentGallery = galleries[project];
+    currentIndex = 0;
+    document.getElementById("galleryImage").src = currentGallery[currentIndex];
+    document.getElementById("galleryModal").style.display = "block";
+}
+
+function closeGallery() {
+    document.getElementById("galleryModal").style.display = "none";
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % currentGallery.length;
+    document.getElementById("galleryImage").src = currentGallery[currentIndex];
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + currentGallery.length) % currentGallery.length;
+    document.getElementById("galleryImage").src = currentGallery[currentIndex];
+}
